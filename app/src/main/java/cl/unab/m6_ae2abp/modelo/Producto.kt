b8a86fc3171ff6b08adf906c6b2118c622cebab1
ALEJANDRA FLAÑO GONZALEZ <a.flaogonzalez@uandresbello.edu>
2025-11-16 18:44:06 -0300

    Revert "Room"

--- app/src/main/java/cl/unab/m6_ae2abp/modelo/Producto.kt
+++ app/src/main/java/cl/unab/m6_ae2abp/modelo/Producto.kt
@@ -1,17 +1,13 @@
 package cl.unab.m6_ae2abp.modelo
 
 import android.os.Parcelable
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-@Entity(tableName = "productos")
 data class Producto(
-    @PrimaryKey(autoGenerate = true) var id: Int = 0,
-    @ColumnInfo(name = "nombre") var nombre: String,
-    @ColumnInfo(name = "descripcion") var descripcion: String,
-    @ColumnInfo(name = "precio") var precio: Int,
-    @ColumnInfo(name = "cantidad") var cantidad: Int
+    var id: Int,
+    var nombre: String,
+    var descripcion: String,
+    var precio: Int,
+    var cantidad: Int
 ) : Parcelable
